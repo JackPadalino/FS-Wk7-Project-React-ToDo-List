dde5c45fa71ec2e5fc5c1b9be24e2d31088eab07
JackPadalino <pada0867@gmail.com>
2022-10-15 11:18:06 -0400

    rendered ToDoItem component in ToDoList componenet - everything working

--- src/toDoItem.js
+++ src/toDoItem.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 const ToDoItem = (props) => {
     return (
-        <div>
-            <tr>
-                <td>{props.task.name}</td>
-                <td>{props.task.status}</td>
-            </tr>
-        </div>
+        <tr>
+            <td>{props.row.name}</td>
+            <td>{props.row.status}</td>
+        </tr>
     );
 };
 
