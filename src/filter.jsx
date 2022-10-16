5e118b038540a63f5ebcb0f21e704e3b320da1e0
JackPadalino <pada0867@gmail.com>
2022-10-16 13:12:49 -0400

    restructured props - have dummy data stored on index.js and then flowing down from filter -> todolist -> todoItem - everything working

--- src/filter.jsx
+++ src/filter.jsx
@@ -1,22 +1,13 @@
 import React,{useState} from 'react';
 import ToDoList from './todoList';
 
-const dummyTasks = [
-    {"id": 1,"name":"Bake a pie"},
-    {"id": 2,"name":"World domination"},
-    {"id": 3,"name":"Complete Fullstack HW"},
-    {"id": 4,"name":"Run for mayor"},
-    {"id": 5,"name":"Practice my head spins"},
-  ];
-
 const Filter = (props) => {
-    const [tasks,setTasks] = useState(dummyTasks);
     return (
         <div id='buttonDiv'>
             <button>All</button>
             <button>Completed</button>
             <button>Active</button>
-            <ToDoList tasks={tasks}/>
+            <ToDoList tasks={props.tasks}/>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import Filter from './filter'; // importing the filter component
 
+const dummyTasks = [
+  {"id": 1,"name":"Bake a pie"},
+  {"id": 2,"name":"World domination"},
+  {"id": 3,"name":"Complete Fullstack HW"},
+  {"id": 4,"name":"Run for mayor"},
+  {"id": 5,"name":"Practice my head spins"},
+];
+
 const Main=()=>{
+  const [tasks,setTasks] = useState(dummyTasks);
   return(
     <div id='toDoList-container'>
       <h1 id="header">To Do List</h1>
-      <Filter/>
+      <Filter tasks={tasks}/>
     </div>
   );
 };
