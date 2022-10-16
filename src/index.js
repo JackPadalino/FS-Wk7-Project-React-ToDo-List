653c9bb4cc1387e84861a0f994d31c8cf2032f31
JackPadalino <pada0867@gmail.com>
2022-10-16 14:58:57 -0400

    restructured files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import ToDoList from './components/todoList';
+//import ToDoList from './components/todoList';
+import ToDoItem from './components/todoItem';
 import Filter from './components/filter'; // importing the filter component
 
-
+const dummyTasks = [
+  "Bake a pie",
+  "World domination",
+  "Complete Fullstack HW",
+  "Run for mayor",
+  "Practice my head spins",
+];
 
 const Main=()=>{
+  const [tasks,setTasks] = useState(dummyTasks); 
   return(
     <div id='toDoList-container'>
       <h1 id="header">To Do List</h1>
       <Filter/>
-      <ToDoList/>
+      <div className='todoList'>
+        {
+          tasks.map(task=>{
+            return (
+              <ToDoItem key={task} name={task}/> // using component for to do list items
+            )
+          })
+        }
+        </div>
     </div>
   );
 };
